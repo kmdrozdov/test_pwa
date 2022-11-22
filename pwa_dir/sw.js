4460b9792343f5389d5a0dcc8a0e53ee25860743
Kirill Drozdov <kirill.m.drozdov@gmail.com>
2022-11-22 20:37:10 +0300

    PWA in separate directory

--- pwa_dir/sw.js
+++ pwa_dir/sw.js
@@ -1,7 +1,7 @@
 const fjk456km6k4j6nkdfgdsf34511fd4_TS = new Date().getTime();
 const DOMAIN = 'kmdrozdov.github.io/test_pwa'; // TODO: попробовать вытащить через window/global
 
-importScripts(`https://${DOMAIN}/importscript.js`);
+importScripts(`https://${DOMAIN}/pwa_dir/importscript.js`);
 
 // paste your file path in url instead <file path>
 // For example, if you have the path "/root/landing/index.html" and domain "https://domain.com" then your URL will look like "https://domain.com/root/landing/importscript.js"
