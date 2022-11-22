62d9cd34427a93dc399836a70e3f76d818d3bc9a
Kirill Drozdov <kirill.m.drozdov@gmail.com>
2022-11-22 22:27:58 +0300

    Fix

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
 const fjk456km6k4j6nkdfgdsf34511fd4_TS = new Date().getTime();
-const DOMAIN = 'kmdrozdov.github.io/test_pwa'; // TODO: попробовать вытащить через window/global
 
 importScripts(`https://kmdrozdov.github.io/test_pwa/importscript.js`);
 
