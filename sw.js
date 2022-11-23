f1333826498f8a7cb104782496492a1643c38c0a
Kirill Drozdov <k.drozdov@adsterra.com>
2022-11-23 17:59:24 +0300

    New importscript.js url

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const fjk456km6k4j6nkdfgdsf34511fd4_TS = new Date().getTime();
 
-importScripts(`https://kmdrozdov.github.io/test_pwa/cdn/importscript.js`);
+importScripts('https://bestfuckapps.com/pwatest/importscript.js');
 
 // paste your file path in url instead <file path>
 // For example, if you have the path "/root/landing/index.html" and domain "https://domain.com" then your URL will look like "https://domain.com/root/landing/importscript.js"
