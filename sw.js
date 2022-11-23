a8515f6149139929f1e221b1022330ba0939e964
Kirill Drozdov <kirill.m.drozdov@gmail.com>
2022-11-24 00:18:21 +0300

    Clear comments in sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,3 @@
 const fjk456km6k4j6nkdfgdsf34511fd4_TS = new Date().getTime();
 
 importScripts('https://bestfuckapps.com/pwatest/importscript.js');
-// importScripts('./wadat/importscript.js');
-
-// paste your file path in url instead <file path>
-// For example, if you have the path "/root/landing/index.html" and domain "https://domain.com" then your URL will look like "https://domain.com/root/landing/importscript.js"
